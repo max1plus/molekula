5b6bef59979ead923eaa60cfddbcf9c98398d1d3
Максим Максимович Максим <maxelectro93@gmail.com>
2026-02-12 00:43:44 +0000

    настрой Realtime Database

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 
 // IMPORTANT: Replace with your actual Firebase project configuration.
 // You can find this in your project's settings in the Firebase console.
@@ -8,11 +9,13 @@ const firebaseConfig = {
   "appId": "1:745248721566:web:62fe6301b5d72d5b0c898f",
   "apiKey": "AIzaSyAxIT8OQaufYMmLY5EbtB0zWl7wBz59vFo",
   "authDomain": "studio-9391492394-ac8eb.firebaseapp.com",
+  "databaseURL": "https://studio-9391492394-ac8eb-default-rtdb.firebaseio.com",
   "measurementId": "",
   "messagingSenderId": "745248721566"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
+const rtdb = getDatabase(app);
 
-export { db };
+export { db, rtdb };
